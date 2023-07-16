ddca755f0fc7e9b44fbb156e4a0aad60f49f8475
Nicolae <nicolae.radu@soft31.eu>
2023-07-16 21:45:29 +0300

    updated path

--- backend/blockchain-service/index.js
+++ backend/blockchain-service/index.js
@@ -514,7 +514,7 @@ if(!process.env.ccp) {
 }else {
   const ccpPath = process.env.ccpPath
 }
-const ccp = JSON.parse(fs.readFileSync("/usr/src/app/connection.org1.json", 'utf8'));
+const ccp = JSON.parse(fs.readFileSync("/usr/src/app/connection-org1.json", 'utf8'));
 createAdmin()
 const port = 3000;
 app.listen(port, () => {
